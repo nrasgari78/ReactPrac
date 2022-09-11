b7c0eaaab13b49945a0af0eabe91c9ec897ff2d5
Your Name <nrasgari78>
2022-09-11 16:26:59 +0430

    20

--- hello/src/Component/Layout/layout.jsx
+++ hello/src/Component/Layout/layout.jsx
@@ -2,12 +2,15 @@ import React, {useEffect, useState} from "react";
 import Navbar from "../Navigation/Navbar/Navbar";
 import ProductsList from "../ProductsList/ProductsList";
 import axios from "axios";
+import Post from "../post/Post";
 const Layout=()=>{
     const [Productsstate,setcProductsstate]=useState({
         products: [{index: 1, title: 'name1', price: 123}, {index: 2, title: 'name2', price: 147}],
         showpro: false,
-        posts:[]
+
     })
+    const [poststate,setpoststate]=useState([])
+
     const removeproductHandler=(key)=>{
         let newproducts= [...Productsstate.products.filter((res)=>res.index!==key)]
         setcProductsstate({products:newproducts})
@@ -16,31 +19,47 @@ const Layout=()=>{
     const changeHandler=()=>{
     alert('eeeert')
      }
-const changeInputHandler=(event)=>{
+    const changeInputHandler=(event)=>{
         console.log('res',event.target.value)
     // let newproducts=  [...Productsstate.products.map((res)=> res['title'] = event.target.value)]
     //
     // setcProductsstate({products: newproducts})
-
 }
     useEffect(()=>{
             axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{
                 console.log(res)
-                // setcProductsstate({posts:res.data})
+                //receive limited data
+                // setpoststate(res.data)
+                const limitedposts=res.data.splice(0,4)
+                const udpatedposts= limitedposts.map((item)=>{
+              return  { ...item,author:'narges'}
+
+
+                })
+                setpoststate(udpatedposts)
             })
         },[]
     )
     return(
     <div>
     <Navbar
-        data={Productsstate.products}
-    />
+        data={Productsstate.products}/>
         <ProductsList
             data={Productsstate.products}
             del={(key)=>removeproductHandler(key)}
             update={changeHandler}
             changeInput={changeInputHandler}
         />
+
+        {poststate.map((item)=> {
+            return (
+            <Post
+            title={item.title}
+            author={item.author}
+            key={item.id}/>)
+             }
+            )}
+
     </div>
     )
 }
---
+++ hello/src/Component/post/Post.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Post=(props)=>{
+    return(
+        <div>
+    <p>title:{props.title}</p>
+    <p>Author:{props.author}</p>
+        </div>
+    )}
+export default Post
